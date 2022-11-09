c57f77f584eceb22cc7964c992cdedec2b707fc2
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-11-08 21:42:46 -0800

    Change import method to fetch

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 // import schedule from './schedule.json' assert {type: 'json'};
-const schedule = require('./schedule.json');
+// const schedule = require('./schedule.json');
+fetch("./schedule.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => render(data));
 
-let root = document.getElementById('root')
-root.innerHTML = "Safari Test"
-for(let i = 0; i < schedule.length; i++){
-    let section = document.createElement('section')
-    let header = document.createElement('h2')
-    header.innerText = schedule[i].name
-    section.appendChild(header)
-    let time = document.createElement('p')
-    time.innerText = schedule[i].time
-    section.appendChild(time)
-    let description = document.createElement('p')
-    description.innerText = schedule[i].description
-    section.appendChild(description)
-    root.appendChild(section)
+function render(schedule) {
+  let root = document.getElementById("root");
+  for (let i = 0; i < schedule.length; i++) {
+    let section = document.createElement("section");
+    let header = document.createElement("h2");
+    header.innerText = schedule[i].name;
+    section.appendChild(header);
+    let time = document.createElement("p");
+    time.innerText = schedule[i].time;
+    section.appendChild(time);
+    let description = document.createElement("p");
+    description.innerText = schedule[i].description;
+    section.appendChild(description);
+    root.appendChild(section);
+  }
 }
