0f6060eb97ee6b7b4c3c0d540fc82be19bfc7aba
MorningPants <77476821+MorningPants@users.noreply.github.com>
2023-01-24 14:20:51 -0800

    test commit to trigger workflow

--- .tina/config.js
+++ .tina/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "tinacms";
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
+
 export default defineConfig({
   branch,
   clientId: "f37fae0d-6025-4fb3-bc3c-a472214afb71", // Get this from tina.io
