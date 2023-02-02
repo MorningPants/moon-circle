e9b43f0b6adaf54579e0c237f4ba3c686e7b992f
Andy <morningpants@gmail.com>
2023-02-02 14:24:55 -0800

    Change CMS to Google Sheets

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 // import schedule from './schedule.json' assert {type: 'json'};
 // const schedule = require('./schedule.json');
-fetch("./schema/schedule.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => render(data));
+// fetch("./schema/schedule.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => render(data));
+
+fetch(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsIJjobZIo0NA-l_J4-tEaT1ZYBj6B4iu3psjqeUgygXY67SjI26pj18-jj8zD1sDbc_ZJOk-NMNtr/pubhtml"
+)
+  .then((response) => response.text())
+  .then((data) => process(data));
+
+function process(data) {
+  
+  let events = [];
+  let event = {}
+  let tempString = "";
+  let passedLine1 = false
+  let passedLine2 = false
+  let hasName = false
+  let hasTime = false
+  let hasDescription = false
+  for (let i = 0; i < data.length; i++) {
+    if (data[i - 3] == "/" && data[i - 2] == "t" && data[i - 1] == "r") {
+      if(passedLine2){
+        events.push(event)
+      }
+      if(passedLine1){
+        passedLine2 = true
+      }
+      passedLine1 = true
+      
+      event = {}
+      hasName = false
+      hasTime = false
+      hasDescription = false
+    }
+    if (data[i - 3] == "<" && data[i - 2] == "t" && data[i - 1] == "d") {
+      while (data[i] !== ">") {
+        i++;
+      }
+      i++;
+      let j = i;
+      while (data[j] + data[j + 1] + data[j + 2] + data[j + 3] !== "</td") {
+        tempString += data[j];
+        j++;
+      }
+      if(!hasName){
+        event.name = tempString;
+        hasName = true
+      } else if(!hasTime){
+        event.time = tempString;
+        hasTime = true
+      } else if(!hasDescription){
+        event.description = tempString
+        hasDescription = true
+      }
+      tempString = "";
+    }
+  }
+  render(events);
+}
 
 function render(schedule) {
   let root = document.getElementById("root");
